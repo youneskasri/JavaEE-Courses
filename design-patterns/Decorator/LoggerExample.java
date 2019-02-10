2ce922338fb08806aec2b02f6ede3d020d5ddc0a
younes <youneskasri@gmail.com>
2019-02-10 17:34:22 +0100

    Decorator in JavaEE

---
+++ design-patterns/Decorator/LoggerExample.java
@@ -0,0 +1,46 @@
+/* Third-party Logged */
+public interface LogMessage {
+	void printMessage();
+	String getMessage();
+	void setMessage(String message);
+}
+
+/* If I want to decorate the Logger, I need to implement the interface 
+Abstract b/c i don't want to implement all the methods 
+*/
+@Decorator
+@Priority(10)
+public abstract class LogMessageFormatter implements LogMessage {
+	
+	@Any
+	@Delegate
+	@Inject
+	private LogMessage logMessage;
+
+	@Override
+	public void printMessage() {
+		String message = logMessage.getMessage();
+		message = LocalDate.now().toString().concat(message);
+		logMessage.setMessage(message);
+	}
+}
+
+@Decorator 
+@Priority(20)
+public abstract class LogMessageJSONFormatter implements logMessage {
+	@Any
+	@Delegate
+	@Inject
+	private  LogMessage logMessage;
+
+	public void printMessage() {
+		String message = logMessage.getMessage();
+		String jsonMessage = 
+JsonbBuilder.create().toJson(message);
+		logMessage.setMessage(jsonMessage);
+	}
+}
+
+/* avec Priority, the timestamp is added first,
+then message is formatted to JSON,
+then the original output method is called */	
