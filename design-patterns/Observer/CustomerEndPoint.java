6efc65ac557c0b2ea58323de2d56cce3b985530e
younes <youneskasri@gmail.com>
2019-02-10 16:32:32 +0100

    Observer priority and Qualifiers

--- design-patterns/Observer/CustomerEndPoint.java
+++ design-patterns/Observer/CustomerEndPoint.java
@@ -2,12 +2,31 @@
 @Path("/customers")
 public class CustomerEndPoint  {
 
-	@Inject
+	@Inject @CustomerEvent(CustomerEvent.Type.ADD)
 	private Event<Customer> customerAddEvent;
+
+	@Inject @CustomerEvent(.. REMOVE)
+	private Event<Customer> customerRemoveEvent;
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void newCustomer(Customer customer) {
 		customerAddEvent.fire(customer);
 	}
+
+	@DELETE
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void removeCustomer(Customer customer) {
+		customerRemoveEvent.fire(customer);
+	}
+}
+
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+@Target((ElementType.FIELD, ElementType.PARAMETER)
+public @interface CustomerEvent {
+
+	Type value();
+	enum Type { ADD, REMOVE }
 }
--- design-patterns/Observer/Observers.java
+++ design-patterns/Observer/Observers.java
@@ -1,19 +1,29 @@
 public class AuthenticationService {
 
-	public void createAuthenticationCredentials(@Observes Customer 
+	public void createAuthenticationCredentials(@Observes 
+@Priority(100)  
+Customer 
 customer) {
 		// create credentials
 	}
 }
 
 public class CustomerService {
-	public void createCustomer(@Observes Customer customer) {
+	public void createCustomer(@Observes @Priority(10)
+@CustomerEvent(CustomerEvent.Type.ADD) Customer 
+customer) {
 		// add new customer
+	}
+
+	public void removeCustomer(@Observers 
+@CustomerEvent(CustomerEvent.Type.REMOVE) Customer customer) {
+		// delete customer
 	}
 }
 
 public class EmailService { 
 
+	/* Default priority = CTE + 500 */
 	public void sendWelcomeEmail(@Observes Customer customer) {
 		// Send welcome email
 	}
